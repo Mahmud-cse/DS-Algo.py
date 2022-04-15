b56a85557c911401d9e42965303fe804aa57ae9f
Mahmud-cse <mahmudhasan555666@gmail.com>
2022-04-16 04:31:10 +0600

    linkedList

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,13 +1,20 @@
 def bubbleSort(arr):
+
     for i in range(len(arr) - 1):
+
         for j in range(len(arr) - i - 1):
+
             if arr[j] > arr[j + 1]:
+
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    print(arr)
+
+    print()(arr)
 
 
 arr = [2, 6, 1, 7, 3]
+
 bubbleSort(arr)
 
 # Time Complexity:: O(n^2)
+
 # Space Complexity:: O(1)
